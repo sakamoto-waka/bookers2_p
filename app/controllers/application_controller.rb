cd2a27488c88713ad758605577e6a24a32221cb7
sakamoto-waka <wakanmhl@gmail.com>
2022-02-17 22:09:46 +0000

    [update] appliction header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # devise由来の機能を使う前に↓が読み込まれる
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # sign_upの際にnameのデータ操作を許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+  end
 end
