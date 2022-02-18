ab8968c47017aa58749c1b2ac651280234c21ee1
sakamoto-waka <wakanmhl@gmail.com>
2022-02-18 04:02:58 +0000

    [update] path for after sign_in,_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   # devise由来の機能を使う前に↓が読み込まれる
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    users_path
+  end  
+  
+  def after_sign_out_path_for(resource)
+    '/'
+  end  
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get 'homes/about' 
+  get 'homes/about' => 'homes#about', as: 'about'
   devise_for :users
   resources :books
   resources :users, only: [:index, :show, :edit]
