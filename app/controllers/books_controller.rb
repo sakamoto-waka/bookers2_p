a20642c5fefd0ddb94d9168d438a0e8aaa7475a8
sakamoto-waka <wakanmhl@gmail.com>
2022-02-23 13:56:30 +0000

    [update] flash messages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-    redirect_to book_path(@book)
+    redirect_to book_path(@book), notice: 'You have created book successfully.'
     else
       render "users/index"
     end
@@ -31,7 +31,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    redirect_to book_path(@book.id)
+    redirect_to book_path(@book.id), notice: 'You have updated book successfully.'
   end
 
   def destroy
