276e21e46c57c45eeed18811f01056e4a2c7c1c1
sakamoto-waka <wakanmhl@gmail.com>
2022-02-24 02:09:52 +0000

    [update] book update error message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,13 +27,15 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    @user = User.find(@book.user_id)
   end
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id), notice: 'You have updated book successfully.'
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'You have updated book successfully.'
+    else
+      render :edit
+    end
   end
 
   def destroy
