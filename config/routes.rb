64d9365d21d58f2989f5b7a48d8c14a599add5a3
sakamoto-waka <wakanmhl@gmail.com>
2022-02-24 05:00:10 +0000

    [start] rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get 'homes/about' => 'homes#about', as: 'about'
+  get 'home/about' => 'homes#about'
   devise_for :users
   resources :books
   resources :users, only: [:index, :show, :edit, :update]
